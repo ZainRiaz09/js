05c0b78c1fdb8261b8e3aa9100d4eee7d9f48ef0
Zain Riaz <149959596+ZainRiaz09@users.noreply.github.com>
2024-02-17 12:09:03 +0500

    Add files via upload

---
+++ 01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 123
+let accountEmail = "zaiin@gmail.com"
+var accountpassword = "12333"
+ 
+// accountcity = "hujra"
+
+// accountId = 2
+
+console.log(accountEmail);
+
+accountEmail = "zain@gmail.com"
+accountPassword = "121"
+accounCity = "okara"
+
+console.table([accountEmail, accountPassword, accounCity]);
