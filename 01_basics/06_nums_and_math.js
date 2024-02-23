0b6aa8829c38ba88f3a486c7f6f2d47c97488d30
Zain Riaz <149959596+ZainRiaz09@users.noreply.github.com>
2024-02-23 21:10:07 +0500

    Add files via upload

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,172 @@
+// const score = 400
+// console.log(score);
+
+
+// const available_balance = new Number (100)
+// console.log(available_balance);
+
+// const available_balance = new Number (100)
+// console.log(available_balance.toString().length);
+// console.log(available_balance.toFixed(4));
+
+// const uumber = 1000000
+// console.log(uumber.toLocaleString('en-IN'));
+
+
+//========================  Maths  ==========================
+
+console.log(Math);
+
+// Math {abs: ƒ, acos: ƒ, acosh: ƒ, asin: ƒ, asinh: ƒ, …}
+// E
+// : 
+// 2.718281828459045
+// LN2
+// : 
+// 0.6931471805599453
+// LN10
+// : 
+// 2.302585092994046
+// LOG2E
+// : 
+// 1.4426950408889634
+// LOG10E
+// : 
+// 0.4342944819032518
+// PI
+// : 
+// 3.141592653589793
+// SQRT1_2
+// : 
+// 0.7071067811865476
+// SQRT2
+// : 
+// 1.4142135623730951
+// abs
+// : 
+// ƒ abs()
+// acos
+// : 
+// ƒ acos()
+// acosh
+// : 
+// ƒ acosh()
+// asin
+// : 
+// ƒ asin()
+// asinh
+// : 
+// ƒ asinh()
+// atan
+// : 
+// ƒ atan()
+// atan2
+// : 
+// ƒ atan2()
+// atanh
+// : 
+// ƒ atanh()
+// cbrt
+// : 
+// ƒ cbrt()
+// ceil
+// : 
+// ƒ ceil()
+// clz32
+// : 
+// ƒ clz32()
+// cos
+// : 
+// ƒ cos()
+// cosh
+// : 
+// ƒ cosh()
+// exp
+// : 
+// ƒ exp()
+// expm1
+// : 
+// ƒ expm1()
+// floor
+// : 
+// ƒ floor()
+// fround
+// : 
+// ƒ fround()
+// hypot
+// : 
+// ƒ hypot()
+// imul
+// : 
+// ƒ imul()
+// log
+// : 
+// ƒ log()
+// log1p
+// : 
+// ƒ log1p()
+// log2
+// : 
+// ƒ log2()
+// log10
+// : 
+// ƒ log10()
+// max
+// : 
+// ƒ max()
+// min
+// : 
+// ƒ min()
+// pow
+// : 
+// ƒ pow()
+// random
+// : 
+// ƒ random()
+// round
+// : 
+// ƒ round()
+// sign
+// : 
+// ƒ sign()
+// sin
+// : 
+// ƒ sin()
+// sinh
+// : 
+// ƒ sinh()
+// sqrt
+// : 
+// ƒ sqrt()
+// tan
+// : 
+// ƒ tan()
+// tanh
+// : 
+// ƒ tanh()
+// trunc
+// : 
+// ƒ trunc()
+// Symbol(Symbol.toStringTag)
+// : 
+// "Math"
+
+
+// console.log(Math.abs(-5)); 
+// console.log(Math.round(5.4));
+// console.log(Math.floor(3.9));
+// console.log(Math.ceil(6.7));
+// console.log(Math.max(3, 4, 5, 5, 8, 1));
+// console.log(Math.min(3, 4, 5, 5, 8, 1));
+
+// console.log(Math.random());
+// console.log((Math.random()*10));
+// console.log((Math.random()*10)+1);
+// console.log(Math.floor((Math.random()*10)+1));
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min );
+
+
+
